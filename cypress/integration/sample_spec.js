baa7e3b560b36731e7415f1c543106a752b2e98d
jcortes <jcortes@switchfly.com>
2019-11-02 13:26:25 -0700

    make failing test on purpose

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -2,6 +2,6 @@ describe('POST', function() {
     it('Should create a new car', function() {
       cy.visit('/')
       cy.get('.btn-primary.my-2').click();
-      cy.get('#createModal button[type=submit]').click();
+      cy.get('#createModal button[type=submitttt]').click();
     })
   })
